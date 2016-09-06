b0c000632dde20a4ec81f1866ed055d93907ccf9
David Zhu <hdavidzhu@gmail.com>
2016-09-06 17:43:53 -0400

    ADD: Basic map with linear probing.

--- misc/generic_hashmap.py
+++ misc/generic_hashmap.py
@@ -2,16 +2,41 @@ class MyHashMap:
 
     def __init__(self, size):
         self.size = size
+        self.table = [None] * size
+        self.count = 0
 
-    def set(key, value):
-        # TODO: Implement.
+    def set(self, key, value):
+        hash_index = hash(key) % self.size
+        for i, val in enumerate(self.table[hash_index:] + self.table[:hash_index]):
+            if val == None or val[0] == key:
+                self.table[(i + hash_index) % self.size] = (key, value)
+                self.count = self.count + 1
+                return True
+        return False
 
-    def get(key):
-        # TODO: Implement.
+    def get(self, key):
+        hash_index = hash(key) % self.size
+        for i, val in enumerate(self.table[hash_index:] + self.table[:hash_index]):
+            if val[0] == key:
+                return val[1]
+        return None
 
-    def delete(key):
-        # TODO: Implement.
+    def delete(self, key):
+        hash_index = hash(key) % self.size
+        for i, val in enumerate(self.table[hash_index:] + self.table[:hash_index]):
+            if val[0] == key:
+                self.table[(i + hash_index) % self.size] = None
+                self.count = self.count - 1
+                return val[1]
 
-    def load():
-        # TODO: Implement.
-        # Items in hashmap / size of hashmap
+    def load(self):
+        return self.count / float(self.size)
+
+if __name__ == "__main__":
+    map = MyHashMap(5)
+    map.set("dog", "Charles")
+    map.set("banana", "Cool")
+    map.set("triangle", "Weird")
+    map.set("car", "Mazarati")
+    map.set("swag", "Money")
+    map.set("noo", "ok")
